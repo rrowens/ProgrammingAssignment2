f5bb683126d8daf49143a5c5466ac017e7c2f99d
Rebecca Owens <rrowens@NA.ds.lexmark.com>
2016-05-18 10:52:29 -0400

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  
+  makeinv = NULL
+  set = function(y) {
+    x <<- y
+    makeinv <<- NULL
+    
+  }
+  get = function() x
+  setinv = function(inverse) makeinv <<- inverse 
+  getinv = function() makeinv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## @x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
+  
+  makeinv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(makeinv)){
+    # get it from the cache and skips the computation. 
+    message("getting cached data")
+    return(makeinv)
+  }
+  
+  # otherwise, calculates the inverse 
+  mat.data = x$get()
+  makeinv = solve(mat.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(makeinv)
+  
+  return(makeinv)
 }
+test = function(mat){
+  ## @mat: an invertible matrix
+  
+  temp = makeCacheMatrix(mat)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
+}
+
